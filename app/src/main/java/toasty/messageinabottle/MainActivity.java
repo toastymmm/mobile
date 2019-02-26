9c628a059b6769656daf75b5b4b9abf2df816bd8
bpglaser <bpglaser@gmail.com>
2019-02-26 14:00:38 -0500

    Added osmdroid example

--- app/src/main/java/toasty/messageinabottle/MainActivity.java
+++ app/src/main/java/toasty/messageinabottle/MainActivity.java
@@ -1,13 +1,46 @@
 package toasty.messageinabottle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private MyLocationNewOverlay myLocationOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.map);
+
+        myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx), mapView);
+        myLocationOverlay.enableMyLocation();
+        mapView.getOverlays().add(myLocationOverlay);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+        myLocationOverlay.disableMyLocation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+        myLocationOverlay.enableMyLocation();
     }
 }
