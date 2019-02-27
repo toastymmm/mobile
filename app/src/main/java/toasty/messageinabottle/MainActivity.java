f078fc9cc4d6f3043563617a0a831fb912627cdf
bpglaser <bpglaser@gmail.com>
2019-02-26 19:37:54 -0500

    Set layout to default on FL, added debug button

--- app/src/main/java/toasty/messageinabottle/MainActivity.java
+++ app/src/main/java/toasty/messageinabottle/MainActivity.java
@@ -4,11 +4,18 @@ import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import org.osmdroid.config.Configuration;
+import org.osmdroid.util.BoundingBox;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.MapView.OnFirstLayoutListener;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import static android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
         myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(ctx), mapView);
         myLocationOverlay.enableMyLocation();
         mapView.getOverlays().add(myLocationOverlay);
+
+        mapView.addOnFirstLayoutListener(new OnFirstLayoutListener() {
+            @Override
+            public void onFirstLayout(View v, int left, int top, int right, int bottom) {
+                mapView.zoomToBoundingBox(new BoundingBox(33.362, -78.343, 24.056, -84.276), false);
+            }
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, mapView.getBoundingBox().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
