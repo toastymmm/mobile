58d46edb3b5dd4ae6c880ac590d283970bdaa0ef
bpglaser <bpglaser@gmail.com>
2019-03-28 17:25:40 -0400

    Implemented the about activity content's

--- app/src/main/java/toasty/messageinabottle/AboutActivity.java
+++ app/src/main/java/toasty/messageinabottle/AboutActivity.java
@@ -1,7 +1,12 @@
 package toasty.messageinabottle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ImageView toastImageView = findViewById(R.id.toastImageView);
+        TextView toastTextView = findViewById(R.id.toastTextView);
+
+        View.OnClickListener gitHubLaunchClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/toastymmm/"));
+                startActivity(intent);
+            }
+        };
+
+        toastImageView.setOnClickListener(gitHubLaunchClickListener);
+        toastTextView.setOnClickListener(gitHubLaunchClickListener);
     }
 }
