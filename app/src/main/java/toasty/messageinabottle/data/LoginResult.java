5985d4a8c4028bb7fd5b56afbd5f705ff87125b2
David Harmeyer <13dharmeyer@gmail.com>
2019-04-11 17:17:50 -0400

    got login and create account working

---
+++ app/src/main/java/toasty/messageinabottle/data/LoginResult.java
@@ -0,0 +1,8 @@
+package toasty.messageinabottle.data;
+
+public enum LoginResult {
+    ACCOUNT_SUCCESSFULLY_CREATED,
+    LOGIN_SUCCESSFUL,
+    USERNAME_ALREADY_EXISTS,
+    INCORRECT_PASSWORD,
+}
--- app/src/main/java/toasty/messageinabottle/io/LiveBackend.java
+++ app/src/main/java/toasty/messageinabottle/io/LiveBackend.java
@@ -1,6 +1,7 @@
 package toasty.messageinabottle.io;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.gson.Gson;
 
@@ -12,10 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.FormBody;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import toasty.messageinabottle.data.LoginResult;
 import toasty.messageinabottle.data.Message;
 import toasty.messageinabottle.data.remote.RemoteMessage;
 
@@ -23,8 +27,13 @@ public class LiveBackend {
 
     private final Gson gson = new Gson();
     private OkHttpClient client;
+    private static LiveBackend backend;
 
-    public LiveBackend(Context ctx) {
+    public static LiveBackend getInstance(Context ctx) {
+        return backend==null?backend=new LiveBackend(ctx):backend;
+    }
+
+    private LiveBackend(Context ctx) {
         PersistentCookieJar cookieJar = new PersistentCookieJar(ctx);
         client = new OkHttpClient.Builder()
                 .readTimeout(0, TimeUnit.MILLISECONDS)
@@ -60,6 +69,66 @@ public class LiveBackend {
                 }
             }
             return result;
+        }
+    }
+
+    public LoginResult attemptLogin(String username, String password) {
+        String tag="in Live Backend: ";
+        RequestBody formBody=new FormBody.Builder()
+                .add("username", username)
+                .add("password", password)
+                .build();
+        Request request=new Request.Builder()
+                .url("http://toastymmm.hopto.org/api/userLogin")
+                .post(formBody)
+                .build();
+
+        try {
+            Response response=client.newCall(request).execute();
+            int code=response.code();
+            String body=response.body().string();
+            Log.d(tag, body);
+            if (code==200) {
+                return LoginResult.LOGIN_SUCCESSFUL;
+            }
+            else {
+                return LoginResult.INCORRECT_PASSWORD;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d(tag, e.toString());
+            return LoginResult.INCORRECT_PASSWORD;
+        }
+    }
+
+    public LoginResult attemptCreateAccount(String username, String password) {
+        String tag="in Live Backend: ";
+        RequestBody formBody=new FormBody.Builder()
+                .add("username", username)
+                .add("password", password)
+                .build();
+        Request request=new Request.Builder()
+                .url("http://toastymmm.hopto.org/api/signup")
+                .post(formBody)
+                .build();
+
+        try {
+            Response response=client.newCall(request).execute();
+            int code=response.code();
+            String body=response.body().string();
+            Log.d(tag, body);
+            if (code==200) {
+                return LoginResult.ACCOUNT_SUCCESSFULLY_CREATED;
+            }
+            else {
+                return LoginResult.USERNAME_ALREADY_EXISTS;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d(tag, e.toString());
+            return LoginResult.INCORRECT_PASSWORD;
         }
     }
 }
